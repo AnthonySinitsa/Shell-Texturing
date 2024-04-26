fedaa507c6a7c067934a77a40e5395ee43701501
Anton <itonyemail@gmail.com>
2024-04-25 19:14:03 -0700

    quad spawns

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,15 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+    public GameObject quadPrefab;
+    public int shellCount = 10;
     public float density = 10f;
 
-    void Update()
+    void Start()
     {
-        Renderer renderer = GetComponent<Renderer>();
-        renderer.material.SetFloat("_Density", density);
+        for (int i = 0; i < shellCount; i++)
+        {
+            // Calculate position for the new quad
+            Vector3 position = transform.position + Vector3.up * i * 0.1f;
+
+            // Instantiate the quad as a child of the prefab
+            GameObject quad = 
+                Instantiate(quadPrefab, position, Quaternion.Euler(90f, 0f, 0f), transform);
+
+            // Set the density value for the quad
+            Renderer renderer = quad.GetComponent<Renderer>();
+            renderer.material.SetFloat("_Density", density);
+        }
     }
 }
